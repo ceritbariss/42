5ee990a26a4a8b3c1c7cda7bab48cd584c9a5743
Baris Cerit <bcerit@k2m16s01.42kocaeli.com.tr>
2022-10-16 20:31:20 +0300

    5

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: bcerit <bcerit@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 16:12:50 by bcerit            #+#    #+#             */
-/*   Updated: 2022/10/16 16:21:37 by bcerit           ###   ########.fr       */
+/*   Updated: 2022/10/16 20:30:42 by bcerit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,20 @@ t_list *ft_lstnew(void *content)
 {
     t_list *new;
 
-    if (!(new = (t_list *)malloc(sizeof(t_list))));
+    if (!(new = (t_list *)malloc(sizeof(t_list))))
         return (0);
     new->content = content;
     new->next = NULL;
     return (new);
 }
+/*
+#include <stdio.h>
+int main()
+{
+    char *content = "baris";
+    t_list *new;
+    new = ft_lstnew((char *)content);
+    
+    printf("%s", new->content);
+}
+*/
